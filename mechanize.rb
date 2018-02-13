7f85b5ece51d94e8fe77bd2527d5d1aac2a5d9ab
Wayne Graham <wgraham@clir.org>
2018-02-13 12:43:29 -0500

    update for walking through posts

--- mechanize.rb
+++ mechanize.rb
@@ -5,7 +5,8 @@ require 'colorize'
 Dotenv.load
 
 @agent = Mechanize.new
-@total_posts = 1045.0
+@total_posts = 280.0
+# @total_posts = 1045.0
 @counter = 0
 
 @agent = Mechanize.new do |agent|
@@ -14,7 +15,7 @@ end
 
 def update_posts(post_list)
   post_list.each do |post|
-    puts "Updating #{post['aria-label']}".light_blue
+    puts "\tUpdating #{post['aria-label']}".light_blue
     @agent.get post['href'] do |post_page|
       form = post_page.form_with(id: 'post')
       button = form.button_with(value: 'Update')
@@ -35,25 +36,39 @@ end
 
 def get_posts(page)
   post_list = page.css('#the-list a[@class="row-title"]')
+  # post_list = page.css('#the-list a[@class="row-title"]')
   update_posts(post_list)
 end
 
 @agent.get(ENV['WORDPRESS_URL']) do |login_page|
 
-  @start = 9
+  @start = 41
+  @end = 53
   puts 'Logging in'.green
   login login_page
 
-  puts 'Navigating to Posts page'.green
-  url = ENV['WORDPRESS_URL'] + "/edit.php?paged=#{@start}"
-  posts_page = @agent.get(url)
+  (@start..@end).each do |p|
 
-  while link = posts_page.at('[class=next-page]')
-    page = @agent.get link[:href]
-    @start += 1
+    url = ENV['WORDPRESS_URL'] + "/edit.php?paged=#{@start}"
+    puts "Navigating to Posts page (#{url})".green
+    posts_page = @agent.get(url)
+
     puts "Working on Posts from page #{@start}".yellow
-    # puts page.title
-    get_posts(page)
+    get_posts(posts_page)
+
+    @start +=1
+
   end
+
+
+
+
+  # while link = posts_page.at('[class=next-page]')
+  #   page = @agent.get link[:href]
+  #   @start += 1
+  #   puts "Working on Posts from page #{@start}".yellow
+  #   puts page.title
+  #   get_posts(page)
+  # end
 
 end
